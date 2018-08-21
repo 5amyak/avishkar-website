8c10af04e9d187b2dc4f419c9b38877b261e91ed
srksumanth <srksumanth@gmail.com>
2018-08-22 00:51:16 +0530

    CORS update

--- server.js
+++ server.js
@@ -4,20 +4,31 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const routes = require("./routes");
 const errorHandler = require("./utils/error-handler");
+const isProduction = require("./utils/isProduction");
+const allowedOrigins = [
+  "https://avishkarmnnit.in",
+  "https://avishkar.mnnit.ac.in"
+];
 mongoose.connect(
   "mongodb://localhost:27017/avishkar",
   { useNewUrlParser: true }
 );
-
 const app = express();
 //CORS handler
 app.use("/api", function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "origin, x-requested-with, content-type, accept,x-access-token,cookie"
-  );
+  if (isProduction) {
+    const origin = req.get("origin");
+    if (!allowedOrigins.includes(origin)) {
+      return res.sendStatus(403);
+    }
+    res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "origin, x-requested-with, content-type, accept,cookie"
+    );
+  }
+
   next();
 });
 app.use("/api", cookieParser());
