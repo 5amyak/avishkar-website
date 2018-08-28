be85d1b369670f24e4c4660ca7b5c97d1d83b862
srksumanth <srksumanth@gmail.com>
2018-08-28 23:56:25 +0530

    update

--- utils/is-authenticated.js
+++ utils/is-authenticated.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 const secret = "secret";
 module.exports = function isAuthenticated(req, res, next) {
-  const token = req.cookies.user || req.get("x-access-token");
+  const token = req.cookies.user;
   if (!token) return res.status(401).json({ message: "Not authenticated" });
 
   jwt.verify(token, secret, function(err, decoded) {
